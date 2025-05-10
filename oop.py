7672c7e26f95b2c43d1a810d891cadf00a51fdfe
SamGon24 <samuelitro24@gmail.com>
2025-05-10 17:30:58 -0500

    Creating characters

---
+++ oop.py
@@ -0,0 +1,20 @@
+class Character:
+    def __init__(self, name, hp, attack):
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+    
+    def is_alive(self):
+        #here we return true if hp is greater than 0
+  
+    def take_damage(self, amount):
+        # we substrat amount from hp
+    def attack(self, other):
+        #make another character take damage
+    
+knight = Character("knight", 55, 100)
+goblin = Character("Goblin", 20, 3)
+
+
+
+
