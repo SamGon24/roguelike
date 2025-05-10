b5b4a23072f3f34c1738dc1bcb8c912afb1b0721
SamGon24 <samuelitro24@gmail.com>
2025-05-10 17:38:09 -0500

    Pushing new changes, adding more logic

--- oop.py
+++ oop.py
@@ -5,15 +5,34 @@ class Character:
         self.attack = attack
     
     def is_alive(self):
-        #here we return true if hp is greater than 0
+        return self.hp > 0
   
     def take_damage(self, amount):
-        # we substrat amount from hp
+        self.hp -= amount
     def attack(self, other):
-        #make another character take damage
-    
+       print(f"{self.name} attacks {other.name} for {self.attack} damage!")
+       other.take_damage(self.attack)    
+       
 knight = Character("knight", 55, 100)
 goblin = Character("Goblin", 20, 3)
+
+round = 1 # round-based game
+while knight.is_alive() and goblin.is_alive():
+    print(f"\n--- Round {round} ---")
+    knight.attack(goblin)
+    print(f"{goblin.name} HP: {goblin.hp}")
+
+    if goblin.is_alive():
+        goblin.attack(knight)
+        print(f"{knight.name} HP: {hero.hp}")
+    
+    round += 1
+
+print ("\n--- Battle Over ----")
+if knight.is_alive():
+    print("{hero.name} wins!")
+else:
+    print("{goblin.name} wins!")
 
 
 
