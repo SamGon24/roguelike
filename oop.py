ccdb5c91ffaa3aff1ec09f1e956263648724b34e
SamGon24 <samuelitro24@gmail.com>
2025-05-11 15:10:22 -0500

    Adding new stuff

--- oop.py
+++ oop.py
@@ -1,22 +1,24 @@
 class Character:
-    def __init__(self, name, hp, attack):
+    def __init__(self, name, hp, attack_power):
         self.name = name
         self.hp = hp
-        self.attack = attack
+        self.attack_power = attack_power 
     
     def is_alive(self):
         return self.hp > 0
   
     def take_damage(self, amount):
         self.hp -= amount
-    def attack(self, other):
-       print(f"{self.name} attacks {other.name} for {self.attack} damage!")
-       other.take_damage(self.attack)    
-       
-knight = Character("knight", 55, 100)
-goblin = Character("Goblin", 20, 3)
 
-round = 1 # round-based game
+    def attack(self, other):
+        print(f"{self.name} attacks {other.name} for {self.attack_power} damage!")
+        other.take_damage(self.attack_power)  # updated here too
+
+# Create characters
+knight = Character("Knight", 55, 100)
+goblin = Character("Goblin", 20, 80)
+
+round = 1
 while knight.is_alive() and goblin.is_alive():
     print(f"\n--- Round {round} ---")
     knight.attack(goblin)
@@ -24,15 +26,16 @@ while knight.is_alive() and goblin.is_alive():
 
     if goblin.is_alive():
         goblin.attack(knight)
-        print(f"{knight.name} HP: {hero.hp}")
+        print(f"{knight.name} HP: {knight.hp}")
     
     round += 1
 
-print ("\n--- Battle Over ----")
+print("\n--- Battle Over ----")
 if knight.is_alive():
-    print("{hero.name} wins!")
+    print(f"{knight.name} wins!")
 else:
-    print("{goblin.name} wins!")
+    print(f"{goblin.name} wins!")
+
 
 
 
